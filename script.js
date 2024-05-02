b5f7f379ed6dd73473a7868b460bc3c9a18b903d
Spencer Payne <37199846+spencerpayne@users.noreply.github.com>
2024-05-02 18:41:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,4 +8,17 @@ function validate() {
         passwordBox.style.border = "red solid 3px";
         return false;
     }
+
+    let email = document.getElementById("email").value;
+    let emailInput = document.getElementById("email");
+
+    let emailRegex =
+    /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    // if statement that tests the student email value with the regex
+    if (emailRegex.test(email) === false) {
+        emailInput.style.border = "red solid 3px";
+        alert('Email not valid');
+        return false;
+    }
 }
