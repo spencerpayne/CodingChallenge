6666ec93800673e9274fe97f0720723c04d649a0
spencerpayne <spencerp52@gmail.com>
2024-05-02 18:42:40 -0500

    .

--- logout.php
+++ logout.php
@@ -1,14 +1,4 @@
 <?php
-// Start the session
-session_start();
-
-// Unset all of the session variables
-$_SESSION = [];
-
-// Destroy the session
-session_destroy();
-
-// Redirect to a login page or any other appropriate page
+// if the user clicks the logout button, take them back to index.html
 header("Location: index.html");
-exit();
 ?>
--- signup.php
+++ signup.php
@@ -1,35 +1,35 @@
 <?php
-$host = "localhost";
-$dbname = "codingchallenge";
-$user = "jpayne";
-$pass = "payne";
+//database connection
+$env = parse_ini_file('.env');
+$host = $env["host"];
+$dbname = $env["dbname"];
+$user = $env["user"];
+$pass = $env["pass"];
 
 $db = pg_connect("host=$host dbname=$dbname user=$user password=$pass");
 if(!$db) {
     echo "Error : Unable to open database\n";
 } else {
-    // Get user input
+    // get input from user
     $username = $_GET["username"];
     $email = $_GET["email"];
     $password = $_GET["password"];
 
-    // Hash the password
+    // hashes the password
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-    // Prepare a parameterized query
+    // prepare the query
     $stmt = pg_prepare($db, "insert_query", "INSERT INTO users (username, email, password) VALUES ($1, $2, $3)");
 
-    // Execute the query with user input as parameters
+    // execute the query with user input
     $result = pg_execute($db, "insert_query", array($username, $email, $hashed_password));
 
     if(!$result) {
-        echo "Error : Unable to register user\n";
+        echo "Error registering user\n";
         echo pg_last_error($db); // Print PostgreSQL error message
     } else {
         echo "User registered successfully\n";
-        // Redirect to a success page or login page
-        // header("Location: success.php");
-        // exit();
+        exit();
     }
 }
 ?>
